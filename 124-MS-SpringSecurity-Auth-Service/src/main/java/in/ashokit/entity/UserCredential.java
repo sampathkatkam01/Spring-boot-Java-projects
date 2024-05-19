7997ea5810080bb68c43337bab1da852a879c406
sampathkatkam <sampathkatkam785@gmail.com>
2024-05-19 19:43:35 +0530

    code updated

---
+++ 124-MS-SpringSecurity-Auth-Service/src/main/java/in/ashokit/entity/UserCredential.java
@@ -0,0 +1,70 @@
+package in.ashokit.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserCredential {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+
+	public UserCredential() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserCredential(int id, String username, String email, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return username;
+	}
+
+	public void setName(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserCredential [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+ "]";
+	}
+	
+	
+
+}
